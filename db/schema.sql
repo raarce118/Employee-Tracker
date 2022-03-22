45bc54f6ee3452d5265b15c60ffe4e8aaa62ed3f
Rich Arce <raarce118@gmail.com>
2022-03-21 21:18:13 -0700

    added seeds, employees, department, roles

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,31 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+
 CREATE DATABASE employees;
 
 USE employees;
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(45) NULL,
     last_name VARCHAR(45) NULL,
-    role_id INT NULL,
+    roles_id INT NULL,
     manager_id INT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(45) NULL,
+    roles_id INT,
+    department_name VARCHAR(45) NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(45) NULL,
-    salary DECIMAL(10.3) NULL,
+    salary DECIMAL NULL,
     department_id INT NULL,
     PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+VALUES
+(1, 'John', 'Doe', '1', '1'),
+(2, 'Mike', 'Chan', '2', '2'),
+(3, 'Ashley', 'Rodriguez', '3', '3'),
+(4, 'Kevin', 'Tupik', '4', '4'),
+(5,'Kunal', 'Singh', '5', '5'),
+(6, 'Malia', 'Brown','1', '1'),
+(7, 'Sarah', 'Lourd','2', '2'),
+(8, 'Tom', 'Allen', '5', '5');
+
+INSERT INTO department(department_name, roles_id)
+VALUES
+('sales', 1),
+('engineering', 2),
+('finance', 3),
+('legal', 4);
+
+INSERT INTO roles(title, salary, department_id)
+VALUES
+('sales lead', '100000', 1),
+('sales person','80000', 1),
+('lead engineer', '150000', 2),
+('software engineer', '120000', 2),
+('account manager', '160000', 3),
+('accountant', '1250000', 3),
+('legal team lead', '250000', 4),
+('lawyer', '190000', 4);
