4be48c2f1bda7d2a2b80216899c43fcf0db8e766
Rich Arce <raarce118@gmail.com>
2022-03-21 20:50:35 -0700

    added more schema

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,17 @@ CREATE TABLE employee (
     manager_id INT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(45) NULL,
+    salary DECIMAL(10.3) NULL,
+    department_id INT NULL,
+    PRIMARY KEY (id)
+);
