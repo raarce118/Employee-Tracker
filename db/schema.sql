77bfc44adb72583a1a23cccefdfa2c7362d679c5
Rich Arce <raarce118@gmail.com>
2022-03-22 22:39:08 -0700

    added console table and inquirer

--- db/schema.sql
+++ db/schema.sql
@@ -6,24 +6,24 @@ USE boss;
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(45) NULL,
-    last_name VARCHAR(45) NULL,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
     roles_id INT NULL,
     manager_id INT NULL,
-    PRIMARY KEY (id)
+    INT PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     roles_id INT,
-    department_name VARCHAR(45) NULL,
-    PRIMARY KEY (id)
+    department_name VARCHAR(30) NULL,
+    INT PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(45) NULL,
+    title VARCHAR(30) NULL,
     salary DECIMAL NULL,
     department_id INT NULL,
-    PRIMARY KEY (id)
+    INT PRIMARY KEY (id)
 );
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const router = express.Router();
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 router.use(require('./rolesRoutes'));
 router.use(require('./departmentRoutes'));
